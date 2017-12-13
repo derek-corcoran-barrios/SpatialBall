cc342673ae7b511a3fcf9a197bb02514cec6694c
derek-corcoran-barrios <derek.corcoran.barrios@gmail.com>
2017-12-13 18:23:22 -0300

    <SpatialballB>

--- R/plots.R
+++ R/plots.R
@@ -452,3 +452,127 @@ DefShotSeasonGraphTeam<- function(Seasondata, team, quant = 0.4, type = "PPS") {
 
   return(GRAPH)
 }
+
+#' plot the shot chart of a whole NBA Season
+#'
+#' This function takes an NBA season object and makes a shot chart of all the
+#' shots takes through that regular season.
+#' You can choose to either plot the results based on Points per Shot or on
+#' Shooting Percentage
+#' @param Seasondata The information of shots, it can be downloaded with function
+#' read_season
+#' @param quant A number between 0 and 1, it determines quantile of shots used
+#' to plot the shot chart, (default is 0.4)
+#' @param type A character to specify if the shot chart is based on Points per
+#' Shot ("PPS") or percentage ("PCT")
+#' @return a ggplot object plotting the shot chart of a given NBA season
+#' @examples
+#' data("season2017")
+#' ShotSeasonGraph(season2017, quant = 0.4)
+#' ShotSeasonGraph(season2017, quant = 0.4, type = "PCT")
+#' @seealso \code{\link[SpatialBall]{DefShotSeasonGraphTeam}}
+#' @seealso \code{\link[SpatialBall]{OffShotSeasonGraphTeam}}
+#' @importFrom dplyr filter
+#' @importFrom ggplot2 aes_string
+#' @importFrom ggplot2 annotation_custom
+#' @importFrom ggplot2 coord_fixed
+#' @importFrom ggplot2 element_blank
+#' @importFrom ggplot2 element_text
+#' @importFrom ggplot2 geom_polygon
+#' @importFrom ggplot2 ggplot
+#' @importFrom ggplot2 ggtitle
+#' @importFrom ggplot2 scale_fill_gradient2
+#' @importFrom ggplot2 theme
+#' @importFrom ggplot2 xlim
+#' @importFrom ggplot2 ylim
+#' @importFrom hexbin hcell2xy
+#' @importFrom hexbin hexbin
+#' @importFrom stats quantile
+#' @author Derek Corcoran <derek.corcoran.barrios@gmail.com>
+#' @export
+
+
+ShotSeasonGraph <- function(Seasondata, quant = 0.4, type = "PPS") {
+  LOC_Y <- ST <- NULL
+  Seasondata <- filter(Seasondata, LOC_Y < 280)
+  #Get the maximum and minumum values for x and y
+  xbnds <- range(Seasondata$LOC_X)
+  ybnds <- range(Seasondata$LOC_Y)
+  #Make hexbin dataframes out of the teams
+
+  if (type == "PPS"){
+    makeHexData <- function(df) {
+      h <- hexbin(df$LOC_X, df$LOC_Y, 25, xbnds = xbnds, ybnds = ybnds, IDs = TRUE)
+      data.frame(hcell2xy(h),
+                 PPS = tapply(as.numeric(as.character(df$SHOT_MADE_FLAG))*ifelse(tolower(df$SHOT_TYPE) == "3pt field goal", 3, 2), h@cID, FUN = function(z) sum(z)/length(z)),
+                 ST = tapply(df$SHOT_MADE_FLAG, h@cID, FUN = function(z) length(z)),
+                 cid = h@cell)
+    }
+  }
+
+  if (type == "PCT"){
+    makeHexData <- function(df) {
+      h <- hexbin(df$LOC_X, df$LOC_Y, 25, xbnds = xbnds, ybnds = ybnds, IDs = TRUE)
+      data.frame(hcell2xy(h),
+                 PPS = tapply(as.numeric(as.character(df$SHOT_MADE_FLAG)), h@cID, FUN = function(z) sum(z)/length(z)),
+                 ST = tapply(df$SHOT_MADE_FLAG, h@cID, FUN = function(z) length(z)),
+                 cid = h@cell)
+    }
+  }
+
+
+  ##Total NBA data
+  Totalhex <- makeHexData(Seasondata)
+  Totalhex <- filter(Totalhex, ST > quantile(Totalhex$ST, probs = quant))
+  Totalhex$ST <- ifelse(Totalhex$ST <= quantile(Totalhex$ST, probs = c(0, 0.25, 0.5, 0.75, 0.9, 1))[2], 0.06,
+                        ifelse(Totalhex$ST > quantile(Totalhex$ST, probs = c(0, 0.25, 0.5, 0.75, 0.9, 1))[2] & Totalhex$ST <= quantile(Totalhex$ST, probs = c(0, 0.25, 0.5, 0.75, 0.9, 1))[3] , 0.12 ,
+                               ifelse(Totalhex$ST > quantile(Totalhex$ST, probs = c(0, 0.25, 0.5, 0.75, 0.9, 1))[3] & Totalhex$ST <= quantile(Totalhex$ST, probs = c(0, 0.25, 0.5, 0.75, 0.9, 1))[4] , 0.25 ,
+                                      ifelse(Totalhex$ST > quantile(Totalhex$ST, probs = c(0, 0.25, 0.5, 0.75, 0.9, 1))[4] & Totalhex$ST <= quantile(Totalhex$ST, probs = c(0, 0.25, 0.5, 0.75, 0.9, 1))[5] , 0.5 ,
+                                             1))))
+
+
+  #Function to transform hexbins into polygons
+  hex_coord_df <- function(x, y, width, height, size = 1) {
+    # like hex_coord but returns a dataframe of vertices grouped by an id variable
+    dx <- size * width / 6
+    dy <- size * height / 2 / sqrt(3)
+
+    hex_y <- rbind(y - 2 * dy, y - dy, y + dy, y + 2 * dy, y + dy, y - dy)
+    hex_x <- rbind(x, x + dx, x + dx, x, x - dx, x - dx)
+    id    <- rep(1:length(x), each=6)
+
+    data.frame(cbind(x=as.vector(hex_x), y=as.vector(hex_y), id))
+  }
+
+  #Transform Hexbins into polygons
+
+  Total <- hex_coord_df(Totalhex$x, Totalhex$y, 35*Totalhex$ST, 12*Totalhex$ST, size =1)
+  Total$PPS <- rep(Totalhex$PPS, each = 6)
+
+  #Make Graph
+  if(type == "PPS"){
+    GRAPH <- ggplot(Total, aes_string(x="x", y = "y"))+ annotation_custom(court, -250, 250, -52, 418) + geom_polygon(aes_string(group = "id", fill = "PPS")) + scale_fill_gradient2(midpoint = 1, low = "blue", high = "red", limits=c(0, 3)) +
+      coord_fixed()  +theme(line = element_blank(),
+                            axis.title.x = element_blank(),
+                            axis.title.y = element_blank(),
+                            axis.text.x = element_blank(),
+                            axis.text.y = element_blank(),
+                            legend.title = element_blank(),
+                            plot.title = element_text(size = 17, lineheight = 1.2, face = "bold")) + ylim(c(-40, 280)) + xlim(c(-250, 250)) + theme(legend.position="bottom")
+  }else{
+    GRAPH <- ggplot(Total, aes_string(x="x", y = "y"))+ annotation_custom(court, -250, 250, -52, 418) + geom_polygon(aes_string(group = "id", fill = "PPS")) + scale_fill_gradient2(midpoint = 0.5, low = "blue", high = "red", limits=c(0, 1)) +
+      coord_fixed()  +theme(line = element_blank(),
+                            axis.title.x = element_blank(),
+                            axis.title.y = element_blank(),
+                            axis.text.x = element_blank(),
+                            axis.text.y = element_blank(),
+                            legend.title = element_blank(),
+                            plot.title = element_text(size = 17, lineheight = 1.2, face = "bold")) + ylim(c(-40, 280)) + xlim(c(-250, 250))+ theme(legend.position="bottom")
+  }
+  if(type == "PPS"){
+    GRAPH <- GRAPH +  ggtitle("Points per Shot")
+  }  else {GRAPH <- GRAPH +  ggtitle("Shooting percentage")}
+
+
+  return(GRAPH)
+}
